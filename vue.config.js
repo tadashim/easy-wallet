bb1d5f762d1e0ed4260a0adf6daecdd12adb2582
tadashim <ma.tadashi@gmail.com>
2020-02-08 17:02:32 +0900

    init repo

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const Dotenv = require('dotenv-webpack')
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/nem-wallet/' : '/',
-  outputDir: 'docs',
-  configureWebpack: {
-    plugins: [new Dotenv()]
-  }
-}
