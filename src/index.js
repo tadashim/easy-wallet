2aa678342b7445554e6888a7c38665f0fc7a066d
tadashim <ma.tadashi@gmail.com>
2020-04-11 16:30:52 +0900

    add amplify app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-const Web3 = require('web3')
-const { setupLoader } = require('@openzeppelin/contract-loader')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-async function main() {
-  const web3 = new Web3('http://localhost:8545')
-  const loader = setupLoader({ provider: web3 }).web3
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-  const address = '0xCfEB869F69431e42cdB54A4F4f105C19C080A601'
-  const box = loader.fromArtifact('Box', address)
-  const accounts = await web3.eth.getAccounts()
-
-  await box.methods.store(20)
-    .send({ from: accounts[0], gas: 50000, gasPrice: 1e6 })
-
-  const value = await box.methods.retrieve().call()
-  console.log("Box value is ", value)
-}
-
-main()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
